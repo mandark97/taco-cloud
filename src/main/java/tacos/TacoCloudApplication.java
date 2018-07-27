437df87db34a5740ce4043aec55f72d2047a5b1b
MELINTE Tudor Matei IMT/OLS <Tudor.Melinte@orange.com>
2018-07-27 14:03:09 +0300

    initial commit

---
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,20 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@SpringBootApplication
+public class TacoCloudApplication extends WebMvcConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
